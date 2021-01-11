fea0a88c7ecd1a8b8b688e7e4f007d81aae87173
ProphetLamb <rene.carannante@outlook.de>
2021-01-11 14:59:02 +0100

    Create CollectionSortHelper

---
+++ src/Utility/CollectionSortHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GroupedObservableCollection.Utility
+{
+    internal static class CollectionSortHelper<T>
+    {
+        // Source: https://source.dot.net/#System.Private.CoreLib/ArraySortHelper.cs,f3d6c6df965a8a86
+        // Modified for IReadOnlyList<T> 
+        public static int BinarySearch(IReadOnlyList<T> array, int index, int length, T value, IComparer<T>? comparer)
+        {
+            if (array is null)
+                throw new ArgumentNullException(nameof(array));
+            if (index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index));
+            if (length < 0)
+                throw new ArgumentOutOfRangeException(nameof(length));
+            if (array.Count < index+length)
+                throw new IndexOutOfRangeException();
+            comparer ??= Comparer<T>.Default;
+            int lo = index;
+            int hi = index + length - 1;
+            while (lo <= hi)
+            {
+                int i = lo + ((hi - lo) >> 1);
+                int order = comparer.Compare(array[i], value);
+                if (order == 0)
+                    return i;
+                if (order < 0)
+                {
+                    lo = i + 1;
+                }
+                else
+                {
+                    hi = i - 1;
+                }
+            }
+
+            return ~lo;
+        }
+    }
+}
